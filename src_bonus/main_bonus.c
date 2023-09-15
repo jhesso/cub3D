eb3b513786d4572ed253294d0fe2bc5dcdf2501e
Dardan <dardangerguri@outlook.com>
2023-09-15 19:34:16 +0300

    Modified some minor thigs

--- src_bonus/main_bonus.c
+++ src_bonus/main_bonus.c
@@ -6,14 +6,16 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 20:01:34 by dgerguri          #+#    #+#             */
-/*   Updated: 2023/09/14 20:29:18 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/09/14 20:38:46 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-int	main()
+int	main(int argc, char **argv)
 {
+	(void)argc;
+	(void)argv;
 	ft_printf(1, "Bonus part!\n");
 	return (0);
 }
