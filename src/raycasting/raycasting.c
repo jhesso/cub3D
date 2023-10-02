f6daf75e428400715bf6719f0cbd32c9ffbaf17c
Dardan <dardangerguri@outlook.com>
2023-10-02 16:12:47 +0300

    Playing with MLX, trying to print it in 2D

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 22:10:33 by dardangergu       #+#    #+#             */
-/*   Updated: 2023/09/30 18:21:55 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/10/02 16:10:23 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,76 @@ int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
     return (r << 24 | g << 16 | b << 8 | a);
 }
 
+// void	draw_box(t_map_data *data, t_vector pos, t_vector size, uint32_t *color)
+// {
+// 	int	x;
+// 	int	y;
+
+// 	y = 0;
+// 	while (y < size.y)
+// 	{
+// 		x = 0;
+// 		while (x < size.x)
+// 		{
+// 			mlx_put_pixel(data->mlx_data->window, pos.x + x, pos.y + y, *color);
+// 			x++;
+// 		}
+// 		y++;
+// 	}
+// }
+
+// static void	static_draw_wall(t_map_data *data, t_vector cord, char type, uint32_t *color)
+// {
+// 	if (type == '1')
+// 		draw_box(data,
+// 			(t_vector){cord.x * 64,  cord.y * 64}, (t_vector){cord.x * 64,  cord.y * 64}, color);
+// 	else if (type == 'O')
+// 		draw_box(data,
+// 			(t_vector){cord.x * 64,  cord.y * 64}, (t_vector){cord.x * 64,  cord.y * 64}, color);
+// }
+
+// static void	draw_walls(t_map_data *data, uint32_t *color)
+// {
+// 	t_vector 	cord;
+
+// 	cord.y = 0;
+// 	cord.x =  0;
+// 	while (data->map[cord.y][cord.x])
+// 	{
+// 		cord.x = 0;
+// 		while (data->map[cord.y][cord.x])
+// 		{
+// 			static_draw_wall(data, cord, data->map[cord.y][cord.x], color);
+// 			cord.x++;
+// 		}
+// 		cord.y++;
+// 	}
+// }
+
 void	raycasting(void *ptr)
 {
 	// (void)ptr;
 	t_map_data *data = (t_map_data *)ptr;
-	printf("starting: %d    %d\n", data->starting_pos.x, data->starting_pos.y);
+	// printf("starting: %d    %d\n", 1920 - (data->starting_pos.x * 64), 1024 - (data->starting_pos.y * 64));
 
+	t_vector cord;
 	uint32_t color = ft_pixel(
 				rand() % 0xFF, // R
 				rand() % 0xFF, // G
 				rand() % 0xFF, // B
 				rand() % 0xFF  // A
 			);
-	mlx_put_pixel(data->mlx_data->window, data->starting_pos.x, data->starting_pos.y, color);
-	// mlx_image_to_window(data->mlx_data->mlx, data->mlx_data->north, 0, 0);
+
+	while (cord.x < 1920)
+	{
+		cord.y = 0;
+		while (cord.y < 1024)
+		{
+			mlx_put_pixel(data->mlx_data->window, cord.x, cord.y, color);
+			cord.y++;
+		}
+		cord.x++;
+	}
+	// draw_walls(data, &color);
 }
+
